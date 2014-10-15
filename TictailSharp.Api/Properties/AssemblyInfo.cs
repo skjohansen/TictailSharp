b54342d5cdeeaea6eada3aabac4964daccf72661
Stephan Johansen <skj@dotmail.dk>
2014-10-15 02:04:08 +0200

    Updated AssemblyInfo

--- TictailSharp.Api/Properties/AssemblyInfo.cs
+++ TictailSharp.Api/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hortuamor.Tictail")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TictailSharp.Api")]
+[assembly: AssemblyDescription(".NET implementation of the Tictail API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Hortuamor.Tictail")]
+[assembly: AssemblyProduct("TictailSharp.Api")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
